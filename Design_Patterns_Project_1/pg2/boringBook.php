5b80a90c91a7a010ff992482dacbc64abdf20f05
Snarna <xyq163446@gmail.com>
2016-11-10 15:11:25 -0500

    WOrking on pg2

---
+++ Design_Patterns_Project_1/pg2/boringBook.php
@@ -0,0 +1,39 @@
+<?php
+class myBoringBook {
+  private $author = "Mingjie Li";
+  private $title = "My Funny Book";
+  private static $book = NULL;
+  private static $isOut = FALSE;
+
+  private function __construct() {}
+
+  static function borrowMyFunBook(){
+    if(self::$isOut == FALSE){
+      if(self::$book == NULL){
+        self::$book = new MyFunBook();
+      }
+      self::$isOut = TRUE;
+      return self::$book;
+    }
+    else{
+      return NULL;
+    }
+  }
+
+  static function returnMyFunBook(MyFunBook $returnedBook){
+    self::$isOut = FALSE;
+  }
+
+  public function getAuthor(){
+    return $this->author;
+  }
+
+  public function getTitle(){
+    return $this->title;
+  }
+
+  public function getAuthorAndTitle(){
+    return $this->title . ' by ' . $this->author;
+  }
+}
+?>
